ecbadac0d04d70620a32d1b873cfd81527aede73
Amod Singh <gamodg@gmail.com>
2020-02-04 11:39:52 +0530

    Updated the project with Actuator and Sample endpoint

--- src/main/java/com/hello/world/HelloWorldApplication.java
+++ src/main/java/com/hello/world/HelloWorldApplication.java
@@ -2,12 +2,21 @@ package com.hello.world;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@GetMapping
+	public ResponseEntity<String> sayHello() {
+		return ResponseEntity.ok("Hello World");
 	}
 
 }
